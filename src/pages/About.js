7036699d22a384a76d4cd9cf4f4475883e448281
faro3328 <faro3328@gmail.com>
2022-05-08 15:35:32 +0200

    creation des fichiers et des dossiers sur React

---
+++ src/pages/About.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident ex
+        inventore eveniet vero saepe. Assumenda, sint. Aliquam saepe doloremque
+        dolorem voluptatem? Sapiente nam sequi molestias, obcaecati blanditiis
+        reiciendis, provident nihil cupiditate officiis tenetur natus ad sed,
+        praesentium commodi assumenda illo.
+      </p>{" "}
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quo error
+        possimus laborum iusto praesentium sapiente cupiditate ducimus impedit
+        fugiat quia totam, labore rerum deserunt, illum dolorum soluta
+        voluptatem, dicta explicabo nulla? Nostrum architecto rem quasi animi
+        provident. Necessitatibus, voluptatem!
+      </p>
+    </div>
+  );
+};
+
+export default About;
